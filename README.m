220d610a59bc26870c553f0ef3e76bc556e7f9e0
Matthewhuleji <hulejimatthew@gmail.com>
2025-12-26 02:36:20 +0100

    Create README for matlab-linalg_solve_system
    Add instructions for running the MATLAB script.

---
+++ README.m
@@ -0,0 +1,9 @@
+# matlab-linalg_solve_system
+
+Contains a simple MATLAB script `matlab-linalg_solve_system.m` that solves a 3x3 linear system and prints the solution.
+
+How to run:
+1. Open MATLAB.
+2. Add the folder to the path or change directory to it.
+3. Run:
+   >> matlab-linalg_solve_system
